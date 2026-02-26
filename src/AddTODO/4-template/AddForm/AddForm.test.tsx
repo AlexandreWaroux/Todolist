d1f6d010275e91f4b658da78667d52bc39aec5d4
Alexandre Waroux <alexwaroux84@gmail.com>
2026-02-26 09:23:52 +0100

    fix : AddForm.test.tsx

--- src/AddTODO/4-template/AddForm/AddForm.test.tsx
+++ src/AddTODO/4-template/AddForm/AddForm.test.tsx
@@ -16,7 +16,7 @@ const setup = (props: AddFormProps) => {
 describe("AddForm component", () => {
   describe("basic", () => {
     it("should render correctly", () => {
-
+      const { component, field, button } = setup({});
       expect(component).toBeInTheDocument();
       expect(field).toBeInTheDocument();
       expect(button).toBeInTheDocument();
@@ -31,7 +31,7 @@ describe("AddForm submit button", () => {
       expect(onSubmitClick).not.toHaveBeenCalled();
 
       fireEvent.click(button);
-      expect(onSubmitClick).not.toHaveBeenCalled();
+      expect(onSubmitClick).toHaveBeenCalled();
     });
   });
 });
